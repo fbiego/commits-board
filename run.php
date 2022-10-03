ac91847e0ca1cf208af35c98d3b635bb200caa20
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-10-03 16:48:18 +0300

    Update run.php

--- run.php
+++ run.php
@@ -9,7 +9,6 @@ function getCountry($country)
     curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
     //curl_setopt($cURLConnection, CURLOPT_HEADERFUNCTION, "getHeaders");
     curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, array(
-        'Accept: application/vnd.github.v3+json',
         'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36'
     ));
 
